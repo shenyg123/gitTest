3aac156483b8c152166053a2c1eb4fb53736c475
shenyg <jacob1107@163.com>
2018-09-17 13:42:37 +0800

    主目录提交

--- src/main/java/com/fuiou/Test.java
+++ src/main/java/com/fuiou/Test.java
@@ -2,9 +2,8 @@ package com.fuiou;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("测试提交");
-		System.out.println("测试提交333");
-		System.out.println("测试提交333");
+	
+		System.out.println("主目录提交");
 	}
 
 }
